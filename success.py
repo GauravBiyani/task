fdb950dc1656f259c7fa9986783bfdf5a64df62f
GauravBiyani <63090049+GauravBiyani@users.noreply.github.com>
2020-05-27 18:47:06 +0530

    initial commit

--- success.py
+++ success.py
@@ -3,6 +3,6 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 s.login("email", "password")
 message = "Hey developer the model is ready and giving good accuracy"
-s.sendmail("ashishbasantani8897@gmail.com", "ashishbasantani77@gmail.com", message)
+s.sendmail("gauravbiyani63@gmail.com", "alphabeast70@gmail.com", message)
 print("Mail Sent")
 s.quit() 
